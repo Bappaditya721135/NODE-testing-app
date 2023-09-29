0dbbea0ff9e03ac2eb88a3aacca7337e97036c9d
Bappaditya sing <104288057+Bappaditya721135@users.noreply.github.com>
2023-09-29 12:13:02 +0530

    testing

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ server.get("/", (req, res) => {
 server.post("/post", async (req, res) => {
     const {name, email, password} = req.body;
     console.log(name, email, password)
-    res.status(200).json({
+    res.status(200).cookie("token", "token").json({
         success: true,
         body: {name: name, email: email, password: password}
     })
