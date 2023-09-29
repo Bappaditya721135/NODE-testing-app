9675d750b565d380c6f17660dd76badcf534a69d
Bappaditya sing <104288057+Bappaditya721135@users.noreply.github.com>
2023-09-29 14:38:06 +0530

    testing

--- server.js
+++ server.js
@@ -19,13 +19,14 @@ server.get("/", (req, res) => {
 })
 
 server.post("/post", async (req, res) => {
-    // const {name, email, password} = req.body;
-    // console.log(name, email, password)
-    // res.status(200).json({
-    //     success: true,
-    //     body: {name: name, email: email, password: password}
-    // })
-    res.send("hello world")
+    const {name, email, password} = req.body;
+    console.log(name, email, password)
+    res.status(200).json({
+        success: true,
+        body: {name: name, email: email, password: password}
+    })
+    // res.send("hello world")
+
 })
 
 
