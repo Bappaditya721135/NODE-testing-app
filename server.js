4512ec7082e47168ee5e2485da68fa26f391105a
Bappaditya sing <104288057+Bappaditya721135@users.noreply.github.com>
2023-09-29 09:53:28 +0530

    doen testing

---
+++ server.js
@@ -0,0 +1,28 @@
+import express from "express";
+
+
+const server = express();
+
+server.use(express.json())
+
+
+server.get("/", (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: "this is the get / page"
+    })
+})
+
+server.post("/post", (req, res) => {
+    const {name, email, password} = req.body;
+    res.status(200).json({
+        success: true,
+        body: {name, email, password}
+    })
+    // res.send("this is post")
+})
+
+
+server.listen(5000, () => {
+    console.log("server is listing")
+})
