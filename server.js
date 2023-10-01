07832e2a508fb605b847acd8dba9a3a59da3e00c
Bappaditya sing <104288057+Bappaditya721135@users.noreply.github.com>
2023-10-01 10:28:35 +0530

    testing doen

--- server.js
+++ server.js
@@ -8,33 +8,45 @@ const server = express();
 server.use(express.json())
 server.use(express.urlencoded({extended: true}))
 server.use(cors({
-    origin: "*",
+    origin: ["http://127.0.0.1:5500"],
     credentials: true,
 }))
 // server.use(bodyParser)
 
 
 server.get("/", (req, res) => {
+    console.log("requsting base url page")
     res.status(200).json({
         success: true,
         message: "this is the get / page"
     })
 })
 
-server.get("/get-cookie", (req, res) => {
-    res.cookie("cookie", "cookie value", {
+server.post("/get-cookie", (req, res) => {
+    console.log("requesting cookie page")
+    // res.cookie("cookie", "cookie value", {
+    //     maxAge: new Date(Date.now() + 600000)
+    // })
+    res.cookie("foo", "bar", {
+        expires: new Date(Date.now() + 60000),
+        httpOnly: true,
         sameSite: "none",
         secure: true,
     })
     res.json({
         "hello": "hello"
     })
+    
 })
 
 server.post("/post", async (req, res) => {
     const {name, email, password} = req.body;
     console.log(name, email, password)
-    res.status(200).json({
+    res.status(200).cookie("token", "token", {
+        expires: new Date(Date.now() + 60000),
+        sameSite: "none",
+        secure: true,
+    }).json({
         success: true,
         body: {name: name, email: email, password: password}
     })
